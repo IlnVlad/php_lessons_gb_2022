0ba608e57f32d11254afa64a99ee75e0e7f7ab10
IlnVlad <95472027+IlnVlad@users.noreply.github.com>
2022-12-08 13:37:51 +1000

    Выполнил HW1
    Выполнил HW1

---
+++ HW1/template_3/index.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Главная страница - страница обо мне";
+$h1 = "Информация обо мне";
+$year = 2022;
+
+$content = file_get_contents("site.html");
+
+$content = str_replace("{{ title }}", $title, $content);
+$content = str_replace("{{ h1 }}", $h1, $content);
+$content = str_replace("{{ year }}", $year, $content);
+echo $content;
